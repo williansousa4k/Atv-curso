6d9c9e60eb66b5caff8ec2dda7b1510a9786d6a7
williansousa4k <williansousa4k@gmail.com>
2023-11-17 21:22:45 -0300

    correção

--- atv-curso.py
+++ atv-curso.py
@@ -1,9 +1,3 @@
-#Desenvolva um programa que recebe do usuário nome completo e ano de nascimento que seja entre 1922 e 2021.
-#A partir dessas informações, o sistema mostrará o nome do usuário e a idade que completou, ou completará, no ano atual (2022).
-
-#Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.
-
-#Atividade do curso
 print('Qual seu nome: ')
 nome = input()
 
